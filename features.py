aabcc60829d482863d056e94a7eed1a672e548bf
Orr Krupnik <orr.krupnik@intel.com>
2017-01-17 22:40:32 +0200

    Initial commit - files and feature module

---
+++ features.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 22:30:07 2017
+
+@author: okrupnik
+"""
+
+"""
+FEATURES module: 
+extracts fetaures from words, parse corpus, save feature tables
+"""
