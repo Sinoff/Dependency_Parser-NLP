60c8c900ace3a5db465b5ba96429c75594f082c1
Orr Krupnik <orr.krupnik@intel.com>
2017-01-22 10:03:58 +0200

    Added inference heuristic

--- inference.py
+++ inference.py
@@ -2,19 +2,37 @@ from features import get_feature_list
 from edmonds import mst
 
 
+def tree_score(tree):
+    score = 0    
+    for parent in tree.keys():
+        for child in tree[parent].keys():
+            score += tree[parent][child]
+    return score
+
+
 def inference(sentence, weights):
-    weights_graph = {}
-    # find best tree
-    for p, parent in enumerate(sentence.words):
-        weights_graph[p] = {}
-        for c, child in enumerate(sentence.words[1:], 1):
-            if p != c:  # cannot have self edges
-                weights_graph[p][c] = -sum(weights[get_feature_list(sentence, p, c)])
-
-
-    # call Edmonds - 0 is root    
-    weights_tree = mst(0, weights_graph)
-    for parent in weights_tree.keys():
-        for child in weights_tree[parent].keys():
+    best_tree = {}
+    for w, word in enumerate(sentence.words[1:], 1):        
+        weights_graph = {}
+        # find best tree
+        for p, parent in enumerate(sentence.words[1:], 1):
+            weights_graph[p] = {}
+            for c, child in enumerate(sentence.words[1:], 1):
+                if p != c:  # cannot have self edges
+                    weights_graph[p][c] = -sum(weights[get_feature_list(sentence, p, c)])
+        
+        # add root node
+        weights_graph[0] = {w : -sum(weights[get_feature_list(sentence, w, c)])}
+    
+        # call Edmonds - 0 is root    
+        weights_tree = mst(0, weights_graph)
+        if w == 1:
+            best_tree = weights_tree
+        else: 
+            if tree_score(best_tree) > tree_score(weights_tree):
+                best_tree = weights_tree
+ 
+    for parent in best_tree.keys():
+        for child in best_tree[parent].keys():
             sentence.add_edge(parent, child)
             
